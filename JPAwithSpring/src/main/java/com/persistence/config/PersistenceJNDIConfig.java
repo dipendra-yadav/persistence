8bc87307233135856e3d6858b977a364ce97ff8d
dipendra-yadav <dipendra.eng@gmail.com>
2017-08-29 12:01:30 +0530

    docs done.

--- JPAwithSpring/src/main/java/com/persistence/config/PersistenceJNDIConfig.java
+++ JPAwithSpring/src/main/java/com/persistence/config/PersistenceJNDIConfig.java
@@ -66,7 +66,7 @@ public class PersistenceJNDIConfig {
 		final Properties hibernateProperties = new Properties();
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 		hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		hibernateProperties.setProperty("hibernate.cache.use_second_level_cache", "false");
+		//hibernateProperties.setProperty("hibernate.cache.use_second_level_cache", "false");
 		return hibernateProperties;
 	}
 }
